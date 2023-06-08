116180cc729962ae3a8e623f76a583d9e3916b9e
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-06-08 13:32:49 +0530

    Adding AnimatePresence and Motion Div

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,34 @@ const Home = () => {
   const snap = useSnapshot(state);
 
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation("left")}>
+        <motion.header {...slideAnimation("down")}>
+          <img src='./threejs.png'alt='logo' className='w-8 h-8 object-contain'/>
+        </motion.header>
+
+        <motion.div className='home-content' {...headContainerAnimation}>
+          <motion.div {...headTextAnimation}>
+            <h1 className='head-text'>
+              LETS'S <br className="x1:block hidden"/> DO IT.
+            </h1>
+          </motion.div>
+
+          <motion.div {...headContentAnimation} className="flec flex-col gap-5">
+            <p className="max-w-md font-normal text-gray-600 text-base">
+            Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style.
+            </p>
+
+            <CustomButton />
+
+          </motion.div>
+
+        </motion.div>
+
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
